7458ed7f849fd0534f6ba5798d35b09670514f08
Sindri P <sindripetur@gmail.com>
2020-11-19 17:18:16 +0100

    Add db-index to Var in Interpreter

--- lib/Interpreter.hs
+++ lib/Interpreter.hs
@@ -17,7 +17,7 @@ isValue TmUnit = True
 isValue _ = False
 
 subst :: Name -> Term -> Term -> Term
-subst x v t@(TmVar n)
+subst x v t@(TmVar n _)
   | x == n = v
   | otherwise = t
 subst x v (TmAbs n ty tm) = TmAbs n ty (subst x v tm)
@@ -25,16 +25,15 @@ subst x v (TmApp tm1 tm2) = TmApp (subst x v tm1) (subst x v tm2)
 subst _ _ TmUnit = TmUnit
 
 interpret :: Term -> Maybe Term
-interpret (TmVar _) = Nothing
+interpret (TmVar _ _) = Nothing
 interpret t@(TmAbs n ty tm) = Just t
 interpret (TmApp (TmAbs n _ v) tm) = Just (subst n v tm)
 interpret (TmApp tm1 tm2) =
-  if isValue tm1 then
-    case interpret tm2 of
+  if isValue tm1
+    then case interpret tm2 of
       Nothing -> Nothing
       Just x -> Just (TmApp tm1 x)
-  else
-    case interpret tm1 of
+    else case interpret tm1 of
       Nothing -> Nothing
       Just x -> Just (TmApp x tm2)
 interpret TmUnit = Just TmUnit
